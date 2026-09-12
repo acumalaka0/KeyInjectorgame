e5fb72faffc659a578ca88c44dc7471a3c05fadb
acumalaka0 <149264768+acumalaka0@users.noreply.github.com>
2026-09-13 03:12:47 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 // config.php
 
-// Token backend / API Key
+// Secret Key Cloudflare Turnstile dari screenshot Anda
+define('TURNSTILE_SECRET_KEY', '0x4AAAAAACzDxGxM5cDqgo3NLwIjJSIta5w');
+
+// Token API Backend
 define('API_TOKEN_RAW', 'Y25LVl9JcURteV9XQ2toU3JKZnJZcDBEZUNwVmdDSTVVeWhtQUE3aFRCdjE0c1dxZWhzcXFEZjBkS0VFV3hCVkFjd19rNVFuWk4teGY5bXE3cllscElHUjRwYU45UkdZSjFLWDNnb1dGSkU=');
 
-// Ganti dengan Secret Key Cloudflare Turnstile Anda dari dashboard Cloudflare
-define('TURNSTILE_SECRET_KEY', '0x4AAAAAACzDxOSs8R4DwJlY_YOUR_SECRET_KEY');
-
-// Fungsi pembantu untuk decode data hex/base64 dari frontend
+// Fungsi pembantu decode data
 function decodeHexBase64($data) {
     if (empty($data)) return '';
     $decodedBase64 = base64_decode($data);
